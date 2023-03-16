4f6fac484ba8fb036255801603d2d1fbd483c75b
SYED <syedali05125569@gmail.com>
2023-03-16 15:07:39 -0400

    Added the api part and the routes done using Router() from express.js

--- controllers/index.js
+++ controllers/index.js
@@ -1,20 +1,22 @@
 const router = require("express").Router();
-// api required routes
-
+//api requires can also be done separately
 const apiRoutes = require("./api");
-
 const indexRoutes = require("./index-routes");
 const loginRoutes = require("./login-routes");
 const postRoutes = require("./post-routes");
-
 const signupRoutes = require("./signup-routes");
-
 const logoutRoutes = require("./logout-routes");
-
 const dashboardRoutes = require("./dashboard-routes");
-
 const editRoutes = require("./edit-routes");
 
+//use each of the routes files in the controllers folder
+router.use("/", indexRoutes);
+router.use("/api", apiRoutes);
+router.use("/login", loginRoutes);
+router.use("/post", postRoutes);
+router.use("/signup", signupRoutes);
+router.use("/logout", logoutRoutes);
+router.use("/dashboard", dashboardRoutes);
+router.use("/edit", editRoutes);
 
-/// useEach of the routes files in the controllers folder
-
+module.exports = router;
