1f8cab493e472958f3cae90d6ae3bafe627a5b55
SYED <syedali05125569@gmail.com>
2023-03-20 14:03:22 -0400

    changed the sql to live sql

--- config/connection.js
+++ config/connection.js
@@ -8,11 +8,11 @@ if (process.env.JAWSDB_URL) {
     sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
     sequelize = new Sequelize(
-        process.env.DB_NAME || "blog_db",
-        process.env.DB_USER || "root",
-        process.env.DB_PASSWORD || "rootr00t!",
+        process.env.DB_NAME,
+        process.env.DB_USER,
+        process.env.DB_PASSWORD,
         {
-            host: "localhost",
+            host: "sql.freedb.tech",
             dialect: "mysql",
             port: 3306,
         }
